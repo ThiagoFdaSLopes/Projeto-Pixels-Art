e2afe21a7870d24473f3ae440f49664fd5bf4653
Thiago <programadorthiagolopes@gmail.com>
2022-07-04 14:56:43 -0300

    feitos os requisitos 6, 7, 8

--- script.js
+++ script.js
@@ -9,3 +9,32 @@ for (let index = 1; index <= 25; index += 1) {
     board.style.width = '220px';
     board.style.height = '220px';
 }
+//--------------------------------------------------------------------
+
+//-----------CRIANDO AS CLASSES SELECTED E NO SELECTED----------------
+function addSelected(event){
+    event.target.classList.add('selected');
+}
+
+let cores = document.getElementsByClassName('color');
+
+function tiraSelect(){
+    for(let i = 0; i < cores.length; i +=1 ){
+        cores[i].classList.remove('selected');
+    }
+}
+
+for(let i = 0; i < cores.length; i += 1){
+    cores[i].addEventListener('click', tiraSelect);
+    cores[i].addEventListener('click', addSelected);
+}
+
+//Colocando a cor do pixel nos pixels
+function coresDosPixels(event) {
+    if(event.target.classList.contains('pixel')){
+        const pegaADiv = document.querySelector('.selected');
+        const pegaACor = window.getComputedStyle(pegaADiv).getPropertyValue('background-Color');
+        event.target.style.backgroundColor = pegaACor;
+    }
+}
+document.addEventListener('click', coresDosPixels);
